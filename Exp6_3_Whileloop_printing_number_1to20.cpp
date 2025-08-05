1f81b0d5099e3e03e74c0a718e714b633db5e0b2
Vidhi Ratan <vidhi.ratan.btech2024@sitpune.edu.in>
2025-08-05 10:53:44 +0530

    Create Exp6_3_Whileloop_printing_number_1to20.cpp

---
+++ Exp6_3_Whileloop_printing_number_1to20.cpp
@@ -0,0 +1,40 @@
+//VIDHI RATAN
+//24070123131
+//B2
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=0;
+    while(i<20){
+        cout<<i+1<<"";
+        i++;
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+/*
+Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+*/
