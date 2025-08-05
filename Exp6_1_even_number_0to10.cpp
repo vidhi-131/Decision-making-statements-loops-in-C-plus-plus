c967e042b15096bfdfe38248946b5fe61bdcb557
Vidhi Ratan <vidhi.ratan.btech2024@sitpune.edu.in>
2025-08-05 10:51:47 +0530

    Create Exp6_1_even_number_0to10.cpp

---
+++ Exp6_1_even_number_0to10.cpp
@@ -0,0 +1,26 @@
+// VIDHI RATAN
+// 24070123131
+// B2
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Even Numbers from 0 to 10 are:"<<endl;
+  for(int i = 0;i <=10;i++){
+      if(i%2==0){
+          cout << i << endl;
+      }
+  }
+  return 0;
+}
+
+/*
+Output:
+Even Numbers from 0 to 10 are:
+0
+2
+4
+6
+8
+10
+*/
