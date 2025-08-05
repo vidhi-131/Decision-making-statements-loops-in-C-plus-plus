f90efca3dee585347ac758d42e2055daf0e7442e
Vidhi Ratan <vidhi.ratan.btech2024@sitpune.edu.in>
2025-08-05 11:11:15 +0530

    Create Exp6_7_SimplePyradmid.cpp

---
+++ Exp6_7_SimplePyradmid.cpp
@@ -0,0 +1,27 @@
+// VIDHI RATAN
+// 24070123131
+// B2
+#include <iostream>
+using namespace std;
+int main() {
+    int n=5;
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<=i;j++){
+            cout<<" * ";
+        
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+/*
+Output:
+ * 
+ *  * 
+ *  *  * 
+ *  *  *  * 
+ *  *  *  *  * 
+*/
