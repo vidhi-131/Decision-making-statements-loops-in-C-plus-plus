f7283525237a6c203e0ce63da2417b703f1aa670
Vidhi Ratan <vidhi.ratan.btech2024@sitpune.edu.in>
2025-08-05 10:52:35 +0530

    Create Exp6_2_forloop_printing_str_5times.cpp

---
+++ Exp6_2_forloop_printing_str_5times.cpp
@@ -0,0 +1,22 @@
+// VIDHI RATAN
+// 24070123131
+// B2
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+  for(int i = 0;i < 5;i++){
+    cout << "SIT "<<endl;
+  }
+  return 0;
+}
+
+/*
+output:
+SIT 
+SIT
+SIT
+SIT
+SIT
+*/
